1754f75384bfc44a29840e0d066ee03d57dce45e
Jimmie Freij <jimfre0823@skola.goteborg.se>
2023-11-21 21:20:04 +0100

    finished the script

--- run_game.sh
+++ run_game.sh
@@ -4,3 +4,23 @@ name="Jimmie_Freij"
 
 echo "$name's program"
 
+mkdir "$name'_labb"
+
+cp *.java "$name'_labb"
+
+cd "$name'_labb"
+
+pwd
+
+echo "compiling..."
+
+javac *.java
+
+echo "running game..."
+
+java GuessingGame
+
+echo "Done!"
+echo "Removing class files..."
+ls
+
