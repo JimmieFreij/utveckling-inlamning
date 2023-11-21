6c3ce2508771de73b3de4119d087b96c99183f91
Jimmie Freij <jimfre0823@skola.goteborg.se>
2023-11-21 20:17:22 +0100

    added code to getReply-method

--- Guesser.java
+++ Guesser.java
@@ -1,4 +1,5 @@
 import java.io.Console;
+import java.util.Scanner;
 
 /**
  * Assignment 1 - Guessing Game
@@ -59,11 +60,18 @@ public class Guesser {
    * the player.
    */
   private String getReply() {
-    String reply = null;
+    String reply = "";
+    Scanner scan = new Scanner(System.in);
     // Write code here which reads a String from the console.
     // As long as it is not a valid reply (one of "T" and "F")
     // write an error message, and read a new reply.
     // When you have gotten a valid reply, return it.
+
+    while (!reply.equals("T") && !reply.equals("F")) {
+      System.out.println("Please answer T or F");
+      reply = scan.nextLine();
+    }
+    
     return reply;
   }
 
