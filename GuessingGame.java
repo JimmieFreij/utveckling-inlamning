4829a964009b300c6acf897066bab4f1918f3c11
Jimmie Freij <jimfre0823@skola.goteborg.se>
2023-11-21 20:19:35 +0100

    changed the intervall to 10

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,6 +1,6 @@
 public class GuessingGame {
     public static void main(String[] args) {
-        Guesser g1 = new Guesser(0,1000);
+        Guesser g1 = new Guesser(0,10);
         g1.start();
     }
 }
